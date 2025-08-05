fafa501e5feaedb26862ef537b40b23ba5bba339
Moussa <m.Khodja@esi-sba.dz>
2025-08-05 10:53:11 +0100

    feat: Add login button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <div className="container m-auto">
-      <div className="text-2xl py-4 border-b">Home</div>
+      <div className="py-4 border-b flex justify-between">
+        <div className="text-2xl">Home</div>
+        <button>Login</button>
+      </div>
       <div>
         Hello Moussa, THIS IS THE HOME PAGE
       </div>
