f7ae57663866a91cda255e5284908dca0fade023
Moussa <m.Khodja@esi-sba.dz>
2025-08-05 14:36:07 +0100

    feat: Add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="container m-auto">
+    <div className="container mx-auto">
       <div className="py-4 border-b flex justify-between">
         <div className="text-2xl">Home</div>
         <div className="flex gap-2">
@@ -8,22 +8,24 @@ export default function Home() {
           <button className="bg-red-300 rounded px-1 text-black">Logout</button>
         </div>
       </div>
-      <div>
-        Hello Moussa, THIS IS THE HOME PAGE
-      </div>
 
+      <div className="h-[calc(100vh-65px-57px)] overflow-auto">
+        <div>
+          Hello Moussa, THIS IS THE HOME PAGE
+        </div>
       <div>
         This is a subtitle Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa totam neque in reprehenderit qui amet esse aperiam. Sed praesentium voluptatem itaque assumenda deserunt, illum, placeat molestias voluptates aspernatur dolorem aut nam quas, veritatis a blanditiis qui corrupti tempora ea doloribus nihil voluptatum. Repellendus recusandae hic numquam, minima aliquid nisi! Tenetur!
       </div>
+        <div className="h-[400px] border flex items-center justify-center mb-2">
+          hero
+        </div>
+        <div className="flex gap-2 basis-6/12 mb-2">
+          <div className="h-[100px] border rounded flex-1"></div>
+          <div className="h-[100px] border rounded flex-1"></div>
+        </div>
 
-      <div className="h-[400px] border flex items-center justify-center mb-2">
-
-        hero
       </div>
-      <div className="flex gap-2 basis-6/12">
-        <div className="h-[100px] border rounded flex-1"></div>
-        <div className="h-[100px] border rounded flex-1"></div>
-      </div>
+      <div className="text-center py-4 border-t">footer</div>
     </div>
   );
 }
