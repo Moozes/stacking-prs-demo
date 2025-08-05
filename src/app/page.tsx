2b04176d2aeb414657493ac9a568d6804d21a8e4
Moussa <m.Khodja@esi-sba.dz>
2025-08-05 11:01:00 +0100

    feat: Add logout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ export default function Home() {
     <div className="container m-auto">
       <div className="py-4 border-b flex justify-between">
         <div className="text-2xl">Home</div>
-        <button>Login</button>
+        <div className="flex gap-2">
+          <button className="px-1">Login</button>
+          <button className="bg-red-300 rounded px-1 text-black">Logout</button>
+        </div>
       </div>
       <div>
         Hello Moussa, THIS IS THE HOME PAGE
